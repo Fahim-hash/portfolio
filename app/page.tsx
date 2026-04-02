fb37edd94e159123eda303311d921cf55d05c740
Fahim-hash <82995127+Fahim-hash@users.noreply.github.com>
2026-04-02 10:10:16 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -624,9 +624,9 @@ const testimonials = [
     
     {/* Warning Section */}
     <p className="text-red-600 text-lg mt-4 animate-pulse font-semibold"> Warning! </p>
-    <h2 className="text-2xl md:text-3xl font-bold tracking-tight mt-1">
+    <h5 className="text-2xl md:text-3xl font-bold tracking-tight mt-1">
       Some accounts may be deactivated. If your inquiry is urgent, please use email to connect.
-    </h2>
+    </h5>
   </div>
 </div>
 
